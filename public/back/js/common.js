fc076842f1e7fa804373dd96a7912f058795f8ca
qiqi <292@qq.com>
2017-11-21 10:31:19 +0800

    初始化提交

---
+++ public/back/js/common.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2017-11-21 10:27:34
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-11-21 10:27:50
+*/
+
+'use strict';
---
+++ public/back/js/login.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Administrator
+* @Date:   2017-11-21 10:27:54
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-11-21 10:28:03
+*/
+
+'use strict';
